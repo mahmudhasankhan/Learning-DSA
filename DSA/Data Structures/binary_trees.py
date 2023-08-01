c752481df1f9a6f343f63d69af6d7d94e64fa725
Khanzaadeh <khanzadeh008@gmail.com>
2023-08-01 22:58:10 +0600

    Binary Tree Implementation Complete, BFS, DFS explored, one lc problem solved

--- DSA/Data Structures/binary_trees.py
+++ DSA/Data Structures/binary_trees.py
@@ -115,8 +115,8 @@ class BinaryTree(object):
     def maximum_depth_with_recursive_dfs(self, root: Node) -> int:
         if root is None:
             return 0
-        return 1 + max(self.maximum_depth(root.left),
-                       self.maximum_depth(root.right))
+        return 1 + max(self.maximum_depth_with_recursive_dfs(root.left),
+                       self.maximum_depth_with_recursive_dfs(root.right))
 
     def maximum_depth_with_bfs(self, root: Node) -> int:
         if root is None:
@@ -124,14 +124,48 @@ class BinaryTree(object):
         queue = deque([root])
         count = 0
         while queue:
+            for i in range(len(queue)):
+                node = queue.pop()
+                if node.left:
+                    queue.appendleft(node.left)
+                if node.right:
+                    queue.appendleft(node.right)
+            count += 1
+        return count
 
-            #           Tree
-            #
-            #            10
-            #           /  \
-            #          2    3
-            #         / \  / \
-            #        4  10 10 20
+    def maximum_depth_with_iterative_dfs(self, root: Node) -> int:
+        if root is None:
+            return 0
+
+        stack = [[root, 1]]
+        res = 1
+
+        while stack:
+            node, depth = stack.pop()
+            if node.left:
+                stack.append([node.left, depth+1])
+            if node.right:
+                stack.append([node.right, depth+1])
+            res = max(res, depth)
+        return res
+
+        #           Tree
+        #
+        #            10
+        #           /  \
+        #          2    3
+        #         / \  / \
+        #        4  10 10 20
+        #       /
+        #     100
+
+        #         New Tree
+        #
+        #            3
+        #           / \
+        #          9   20
+        #             /  \
+        #            15   7
 
 
 def main():
@@ -142,13 +176,25 @@ def main():
     tree.root.left.right = Node(10)
     tree.root.right.left = Node(10)
     tree.root.right.right = Node(20)
+    tree.root.left.left.left = Node(100)
 
-    print(f" Pre-order: {tree.print_tree('preorder')}")
-    print(f" In-order: {tree.print_tree('inorder')}")
-    print(f" Post-order: {tree.print_tree('postorder')}")
-    print(f" Level-order: {tree.print_tree('levelorder')}")
-    print(f" Level-order-deque: {tree.print_tree('levelorder-deque')}")
-    print(f"Maximum Depth of a tree: {tree.maximum_depth(tree.root)}")
+    new_tree = BinaryTree(3)
+    new_tree.root.left = Node(9)
+    new_tree.root.right = Node(20)
+    new_tree.root.right.left = Node(15)
+    new_tree.root.right.right = Node(7)
+
+    print(f" Pre-order: {new_tree.print_tree('preorder')}")
+    print(f" In-order: {new_tree.print_tree('inorder')}")
+    print(f" Post-order: {new_tree.print_tree('postorder')}")
+    print(f" Level-order: {new_tree.print_tree('levelorder')}")
+    print(f" Level-order-deque: {new_tree.print_tree('levelorder-deque')}")
+    print(
+        f"Maximum Depth of a tree with recursive dfs: {new_tree.maximum_depth_with_recursive_dfs(new_tree.root)}")
+    print(
+        f"Maximum Depth of a tree with bfs: {new_tree.maximum_depth_with_bfs(new_tree.root)}")
+    print(
+        f"Maximum Depth of a tree with iterative dfs: {new_tree.maximum_depth_with_iterative_dfs(new_tree.root)}")
 
 
 if __name__ == "__main__":
