455fc481655d576be8705692f0638f1d5b53ad0b
mahmudhasankhan <khanzadeh008@gmail.com>
2024-01-14 18:37:48 +0600

    Repitition

---
+++ DSA/CSES/repetitions.py
@@ -0,0 +1,11 @@
+def main():
+    s = input()
+    # two pointer approach
+
+    l_ptr, r_ptr = 0, 1
+    while r_ptr <= len(s):
+        if s[l_ptr] = s[r_ptr]:
+
+
+if __name__ == "__main__":
+    main()
