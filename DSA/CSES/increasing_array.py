05a4a74548c63e94b98bcdc6df765346dec6eff0
mahmudhasankhan <khanzadeh008@gmail.com>
2024-01-17 18:10:46 +0600

    Increasing array done

--- DSA/CSES/increasing_array.py
+++ DSA/CSES/increasing_array.py
@@ -1,15 +1,13 @@
 def main():
     n = int(input())
     l = [int(x) for x in input().split()]
-    step = 0
-    for i in range(n):
-        for k in range(1, n-1):
-            if l[i] > l[k]:
-                step += 1
-                temp = l[i]
-                l[i] = l[k]
-                l[k] = temp
-    print(step)
+    moves = 0
+    for i in range(1, n):
+        if l[i] < l[i-1]:
+            diff = l[i-1] - l[i]
+            l[i] += diff
+            moves += diff
+    print(moves)
 
 
 if __name__ == "__main__":
