49fb441010d11977f7dc4402163fd5fd3d01f002
mahmudhasankhan <khanzadeh008@gmail.com>
2024-03-11 18:21:48 +0600

    cses: two sets

---
+++ DSA/CSES/twosets.py
@@ -0,0 +1,15 @@
+
+def main():
+    n = int(input())
+    sum = n*(n+1)//2
+
+    if sum % 2 != 0:
+        print("NO")
+        return
+    else:
+        print("YES")
+    print("come")
+
+
+if __name__ == "__main__":
+    main()
